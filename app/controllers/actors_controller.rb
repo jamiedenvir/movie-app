7d20a28e216ee8e3590114cf95b63c0b623853de
jamie denvir <jwdenvir@gmail.com>
2021-05-06 09:00:56 -0400

    creates actors controller and associated route to display based on id

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,4 +1,7 @@
 class ActorsController < ApplicationController
-  
+  def get_id_method
+    actor = Actor.find_by(id: 1)
+    render json: actor.as_json
+  end
    
 end
--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,5 +2,3 @@ class Actor < ApplicationRecord
   
 
 end
-
-#testing
