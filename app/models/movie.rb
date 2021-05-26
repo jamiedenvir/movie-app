ead79f94594641e1ab14a77193e13d07ac2adbe5
jamie denvir <jwdenvir@gmail.com>
2021-05-26 08:32:45 -0400

    adds has many through associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,12 @@
 class Movie < ApplicationRecord
   
+  
   has_many :actors
+
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres 
+
+
   
   validates :title, presence: true, length: { minimum: 2}
 
