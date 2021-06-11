cf6ce380ee45432cdaba85b59269dbe50961a102
jamie denvir <jwdenvir@gmail.com>
2021-06-11 08:51:40 -0400

    changed spelling error session to sessions

--- config/routes.rb
+++ config/routes.rb
@@ -41,7 +41,7 @@ Rails.application.routes.draw do
 
   post "/users" => "users#create"
 
-  post "/session" => "sessions#create"
+  post "/sessions" => "sessions#create"
 
   post "/movie_genres" => "movie_genres#create"
 
